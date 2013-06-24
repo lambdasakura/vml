37ceda460dd63e0f3f63479c7123915d0a8810e0
lambda_sakura <lambda.sakura@gmail.com>
2013-06-24 17:32:49 +0900

    joystick api update

--- input/joystick.lisp
+++ input/joystick.lisp
@@ -12,94 +12,59 @@
 (in-package :vml-joystick)
 (cl-annot:enable-annot-syntax)
 
-@export
 (defparameter *joystick-manager* nil)
 
-@export
-(defun init-joystick ()
-  (setf *joystick-manager*
-	(make-instance 'joystick-manager)))
-
-@export
-(defun handle-cursor-event (axis value index)
-  (update-cursor *joystick-manager* axis value index))
-  
-@export
-(defun handle-button-press-event (button index)
-  (button-press *joystick-manager* button index))
-
-@export
-(defun handle-button-release-event (button index)
-  (button-release *joystick-manager* button index))
-
-@export
-(defun joystick-update ()
-  (let ((joystick (aref (joystick *joystick-manager*) 0)))
-    (when (not (= 0 joystick))
-      (loop for key in '(:up :down :left :right
-			 :button0 :button1 :button2 :button3 :button4
-			 :button5 :button6 :button7 :button8
-			 :button9 :button10 :button11) do
-	   (update-prev-and-current (joystick-state joystick) key)))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; button current state
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-@export
-(defun joystick-button-state (button
-			      &optional (index 0))
-  (cond ((> (num-joysticks *joystick-manager*) 0)
-	 (gethash button (state (joystick-state (aref (joystick *joystick-manager*) index)))))
-	(t
-	 nil)))
-
-
-@export
-(defun joystick-button-pushed (button
-				   &optional (index 0))
-  (cond ((> (num-joysticks *joystick-manager*) 0)
-	 (gethash button (pushed (joystick-state (aref (joystick *joystick-manager*) index)))))
-	(t
-	 nil)))
-
-@export
-(defun joystick-button-press (button
-				   &optional (index 0))
-  (cond ((> (num-joysticks *joystick-manager*) 0)
-	 (gethash button (press (joystick-state (aref (joystick *joystick-manager*) index)))))
-	(t
-	 nil)))
-
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; ジョイスティックのデバイスに依存する情報
-;; SDLに依存している部分
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defclass joystick-device-info ()
- ((fp :initform nil :accessor fp :initarg :fp)
-   (index :initform nil :accessor index :initarg :index)))
-
-(defmethod initialize-instance :after ((self joystick-device-info) &rest arg)
-  (declare (ignore arg))
-  (setf (fp self) (sdl-cffi::sdl-joystick-open (index self)))
-  (format t "~A~%" (sdl-cffi::sdl-joystick-name (index self)))
-  (format t "~A~%" (fp self))
-)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; ジョイスティックがいまどうなっているかを表す
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defclass joystick-state ()
-  ((state :initform (make-hash-table :test 'equal) :accessor state)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 現在のジョイスティックの状態を保持するクラス
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defclass joystick-manager ()
+  ((num-joysticks :initform 0 :accessor num-joysticks)
+   (joystick :initform nil :accessor joystick :initarg :joystick)))
+(defclass joystick ()
+  ((fp :initform nil :accessor fp :initarg :fp)
+   (state :initform (make-hash-table :test 'equal) :accessor state)
    (current :initform (make-hash-table :test 'equal) :accessor current)
    (prev :initform (make-hash-table :test 'equal) :accessor prev)
    (pushed :initform (make-hash-table :test 'equal) :accessor pushed)
    (press :initform (make-hash-table :test 'equal) :accessor press)))
 
-(defmethod update-joystick-state ((self joystick-state) button next-state)
-  (let ((state (state self)))
-    (setf (gethash button state) next-state)))
+(defmethod initialize-instance :after ((self joystick) &rest arg)
+  (declare (ignore arg))
+  (setf (fp self) (sdl-cffi::sdl-joystick-open (index self))))
+
+(defmethod initialize-instance :after ((self joystick-manager) &rest arg)  
+  (declare (ignore arg))
+  ;;  Joystickのイベントを受け取れるようにSDLの関数をcall
+  (sdl-cffi::sdl-joystick-event-state sdl-cffi::sdl-enable)
+  (let ((num-joysticks (sdl:num-joysticks))
+	;; ジョイスティックの数を登録
+	(setf (num-joysticks self) num-joysticks)
+	
+	;; すべてのジョイスティックを初期化
+	(cond ((> num-joysticks 0)
+	       (setf (joystick self)
+		     (loop for i=0 to num-joysticks
+			collect (device-info (make-instance 'joystick-device-info :index i))))
+	       (t nil))))))
+
+(defun close-joystick (joystick)
+  (when (> (sdl-cffi::sdl-joystick-opened (joystick-index joystick)) 0)
+    (sdl-cffi::sdl-joystick-close (joystick-fp joystick))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; System API
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun init-joystick ()
+  (setf *joystick-manager* (make-instance 'joystick-manager)))
+
+(defun handle-cursor-event (axis value index)
+  (update-cursor *joystick-manager* axis value index))
+
+(defun handle-button-press-event (button index)
+  (button-press *joystick-manager* button index))
+
+(defun handle-button-release-event (button index)
+  (button-release *joystick-manager* button index))
 
 (defmethod update-prev-and-current ((self joystick-state) key)
   (let ((state (state self))
@@ -111,19 +76,23 @@
     (setf (gethash key prev) (gethash key current))
     (setf (gethash key current) (gethash key state))
 
-    (setf (gethash key pushed) 
-	  (and (eq nil (gethash key prev))
-	       (eq t (gethash key current))))
+    (setf (gethash key pushed) (and (eq nil (gethash key prev)) (eq t (gethash key current))))
     (setf (gethash key press) (gethash key current))))
-  
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; joystick自身
-;; デバイスに関する情報、ボタンの状態を保持
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defclass joystick ()
-  ((device-info :initform nil :accessor device-info :initarg :device-info)
-   (joystick-state :initform (make-instance 'joystick-state) :accessor joystick-state)))
 
+(defun joystick-update ()
+  (let ((joystick (aref (joystick *joystick-manager*) 0)))
+    (when (not (= 0 joystick))
+      (loop for key in '(:up :down :left :right
+			 :button0 :button1 :button2 :button3 :button4
+			 :button5 :button6 :button7 :button8
+			 :button9 :button10 :button11) do
+	   (update-prev-and-current (joystick-state joystick) key)))))
+
+(defmethod update-cursor ((self joystick-manager) AXIS VALUE &optional (index 0))
+  (let ((joystick (aref (joystick self) index)))
+    (when joystick
+      (update-joystick-axis joystick axis value))))
+  
 (defmethod update-joystick-axis ((self joystick) axis value)
   (cond ((= axis 0)
 	 (update-joystick-vertical self value))
@@ -148,45 +117,13 @@
 	  ((<= value -512)
 	   (update-joystick-state state :up t)))))
 
-@export
-(defun close-joystick (joystick)
-  (when (> (sdl-cffi::sdl-joystick-opened (joystick-index joystick)) 0)
-    (sdl-cffi::sdl-joystick-close (joystick-fp joystick))))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; joystick manager
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defmethod update-joystick-state ((self joystick) button next-state)
+  "ボタンの状態を引数の状態に更新する"
+  (setf (gethash button (state self)) next-state))
 
-@export 
-(defclass joystick-manager ()
-  ((num-joysticks :initform 0 :accessor num-joysticks)
-   (index :initform 0 :accessor index :initarg :index)
-   (joystick :initform (make-array 4) :accessor joystick :initarg :joystick)))
-
-(defmethod initialize-instance :after ((self joystick-manager) &rest arg)  
-  (declare (ignore arg))
-  (sdl-cffi::sdl-joystick-event-state sdl-cffi::sdl-enable)
-  (setf (index self) 0)
-  (let* ((num-joysticks (sdl:num-joysticks))
-	 (device-info (make-instance
-		       'joystick-device-info :index (index self))))
-    (setf (num-joysticks self) num-joysticks)
-    (format t "~A~%" (sdl:num-joysticks))
-    (cond ((> num-joysticks 0)
-	   (setf (aref (joystick self) (index self))
-		 (make-instance 'joystick :device-info device-info)))
-	  (t 
-	   nil))))
-
-  
-@export
-(defmethod update-cursor ((self joystick-manager) AXIS VALUE &optional (index 0))
-  (let ((joystick (aref (joystick self) index)))
-    (when joystick
-      (update-joystick-axis joystick axis value))))
-
-@export
 (defmethod button-press ((self joystick-manager) button &optional (index 0))
+  "ボタンが押された時にボタンの状態を更新する関数"
   (let* ((joystick (aref (joystick self) index))
 	 (state (joystick-state joystick)))
     (when joystick
@@ -202,11 +139,10 @@
 	(8 (update-joystick-state state :button8 t))
 	(9 (update-joystick-state state :button9 t))
 	(10 (update-joystick-state state :button10 t))
-	(11 (update-joystick-state state :button11 t))
-	))))
+	(11 (update-joystick-state state :button11 t))))))
 
-@export
 (defmethod button-release ((self joystick-manager) button &optional (index 0))
+  "ボタンが離された時にボタンの状態を更新する関数"
   (let* ((joystick (aref (joystick self) index))
 	 (state (joystick-state joystick)))
     (when joystick
@@ -222,7 +158,25 @@
 	(8 (update-joystick-state state :button8 nil))
 	(9 (update-joystick-state state :button9 nil))
 	(10 (update-joystick-state state :button10 nil))
-	(11 (update-joystick-state state :button11 nil))
-	))))
+	(11 (update-joystick-state state :button11 nil))))))
 
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; User API
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun joystick-button-state (button &optional (index 0))
+  (cond ((> (num-joysticks *joystick-manager*) 0)
+	 (gethash button (state (aref (joystick *joystick-manager*) index))))
+	(t nil)))
+
+(defun joystick-button-pushed (button
+				   &optional (index 0))
+  (cond ((> (num-joysticks *joystick-manager*) 0)
+	 (gethash button (pushed (aref (joystick *joystick-manager*) index))))
+	(t nil)))
+
+(defun joystick-button-press (button
+				   &optional (index 0))
+  (cond ((> (num-joysticks *joystick-manager*) 0)
+	 (gethash button (press (aref (joystick *joystick-manager*) index))))
+	(t nil)))
